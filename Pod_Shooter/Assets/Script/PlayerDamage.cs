f90abd0ca117237aa5d9e5d2db45c10f1a096861
B533P-09\533 <simoroy94@gmail.com>
2018-11-05 16:41:15 -0500

    ajout du multiplayer a POD shooter

---
+++ Pod_Shooter/Assets/Script/PlayerDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour,Damagable {
+    PlayerLife playerLife;
+
+    public void DealDamage(int Damage)
+    {
+        playerLife.TakeDamage(Damage);
+    }
+
+    // Use this for initialization
+    void Start () {
+        playerLife = GetComponent<PlayerLife>();
+	}
+	
+}
